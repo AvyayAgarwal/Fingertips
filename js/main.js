7ee5f56780922e44761b7f5ba564be8339c4e43a
Sara Fong <sarafong19@gmail.com>
2017-09-16 12:30:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,5 +7,18 @@ function init_main () {
     $('html').hide().fadeIn('slow');
 }
 
+var siteToSearch = 'reddit.com';
+
+chrome.history.search({text: siteToSearch, callback: function(results) {
+
+	
+
+}}
+
+chrome.history.getVisits(object details, function callback){
+
+
+}
+
 //bind events to dom elements
 document.addEventListener('DOMContentLoaded', init_main);
